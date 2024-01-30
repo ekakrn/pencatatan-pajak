170239792e0ae12b4f154a3e41c005199192168d
PratiwiMh <pratiwimh1203@gmail.com>
2024-01-30 10:03:53 +0700

    menambahkan datatables

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,8 +10,9 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
             rel="stylesheet"
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.css" />
     </head>
-    <body>
+    <body class="bg-light">
         <div id="wrapper">
             <div id="content-wrapper" class="d-flex flex-column">
                 <div id="content">
@@ -29,7 +30,7 @@
                                     </li>
                                 </ul>
                                 <form class="d-flex">
-                                    <button class="btn btn-outline-danger" type="button">Logout</button>
+                                    <a class="btn btn-outline-danger" href="/signout">Logout</a>
                                 </form>
                             </div>
                         </div>
@@ -46,6 +47,15 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+        crossorigin="anonymous"></script>
+        <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
+        <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.js"></script>
+
+        <script>
+            let table = new DataTable('#dataTable');
+        </script>
 
     </body>
 </html>
