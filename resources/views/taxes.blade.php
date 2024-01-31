47172f548b1d2eae4ca73539cc7699e08851b18d
PratiwiMh <pratiwimh1203@gmail.com>
2024-01-31 10:21:48 +0700

    membuat tombol hapus

--- resources/views/taxes.blade.php
+++ resources/views/taxes.blade.php
@@ -4,9 +4,9 @@
 
 <!-- Page Heading -->
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">Pajak</h1>
-    <a href="/add-page" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
-        <i class="fas fa-plus fa-sm text-white-50"></i> Tambah Data
+    <h1 class="h3 mb-0 text-gray-800">Data Pajak</h1>
+    <a href="/add-page" class="btn btn-primary">
+        Tambah Data
     </a>
 </div>
 
@@ -18,29 +18,38 @@
                 <caption></caption>
                 <thead>
                     <tr>
-                        <th id="thPenjualan" style="width:15%">Penjualan</th>
-                        <th id="thBebanAdm" style="width:10%">Beban Adm</th>
-                        <th id="thBebanPemasaran" style="width:10%">Beban Pemasaran</th>
-                        <th id="thBebanLain" style="width:10%">Beban Lain</th>
-                        <th id="thPendapatanLain" style="width:10%">Pendapatan Lain</th>
-                        <th id="thTotal" style="width:15%">Total</th>
-                        <th id="thTotalDenganPajak" style="width:15%">Total Dengan Pajak</th>
-                        <th id="thActionHeader" style="width:15%">Action</th>
+                        <th class="text-center" id="thPenjualan" style="width:15%">Penjualan</th>
+                        <th class="text-center" id="thBebanAdm" style="width:15%">Beban</th>
+                        <th class="text-center" id="thPendapatanLain" style="width:15%">Pendapatan Lain</th>
+                        <th class="text-center" id="thTotal" style="width:15%">Total</th>
+                        <th class="text-center" id="thTotalDenganPajak" style="width:15%">Pajak (22%)</th>
+                        <th class="text-center" id="thActionHeader" style="width:25%">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($taxes as $data)
 
-                        <tr>
+                        <tr class="text-center">
                             <td>{{ $data->penjualan }}</td>
-                            <td>{{ $data->beban_administrasi }}</td>
-                            <td>{{ $data->beban_pemasaran }}</td>
-                            <td>{{ $data->beban_lainnya }}</td>
+                            <td>
+                                <div> Administrasi: {{ $data->beban_administrasi }}</div>
+                                <div> Pemasaran: {{ $data->beban_pemasaran }}</div>
+                                <div> Lainnya: {{ $data->beban_lainnya }}</div>
+                            </td>
                             <td>{{ $data->pendapatan_lain }}</td>
                             <td>{{ $data->total }}</td>
                             <td>{{ $data->total_pajak }}</td>
                             <td>
-
+                                <a href="/export-excel" class="btn btn-success mb-1">Excel</a>
+                                <a href="/edit-page" class="btn btn-primary mb-1">Ubah</a>
+                                <form method="post" class="d-inline"
+                                    title="Delete" action="/delete-data/{{ $data->id }}">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+                                    <button type="submit" class="btn btn-danger">
+                                        Hapus
+                                    </button>
+                                </form>
                             </td>
                         </tr>
 
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/add-data', [TaxController::class, 'addData']);
+Route::delete('/delete-data/{id}', [TaxController::class, 'deleteData']);
+Route::post('/add-data', [TaxController::class, 'addData']);
 Route::get('/add-page', [TaxController::class, 'addPage']);
 Route::get('/', [TaxController::class, 'index']);
 Route::get('/signout', [AuthController::class, 'signOut']);
